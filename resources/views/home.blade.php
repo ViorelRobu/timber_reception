b05a72e99fa25efbb52898a267c35b1854149214
Viorel Robu <viorel.robu@schweighofer.ro>
2019-11-27 17:51:05 +0200

    bugfix dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,23 +18,40 @@
     <div class="row">
         <div class="col-lg-3 text-center">
             <h3>Pret mediu luna curenta</h3>
-            <h4>{{ round(($current_month['valoare'] / $current_month['volum']),2) }} EUR</h4>
+            @if ($current_month['valoare'] == 0)
+                <h4>Nu exista date</h4>
+            @else
+                <h4>{{ round(($current_month['valoare'] / $current_month['volum']),2) }} EUR</h4>
+            @endif
+            
         </div>
         <div class="col-lg-3 text-center">
             <h3>Pret mediu luna curenta/furnizor</h3>
-            @foreach ($data_current_month as $data)
-                <h4>{{ $data->supplier }} &mdash; {{ round((($data->valoare_factura + $data->transport)/$data->volum),2) }} EUR</h4>
-            @endforeach
+            @if ($data_current_month->count() == 0)
+                <h4>Nu exista date</h4>
+            @else
+                @foreach ($data_current_month as $data)
+                    <h4>{{ $data->supplier }} &mdash; {{ round((($data->valoare_factura + $data->transport)/$data->volum),2) }} EUR</h4>
+                @endforeach
+            @endif
         </div>
         <div class="col-lg-3 text-center">
             <h3>Pret mediu luna anterioara</h3>
-            <h4>{{ round(($last_month['valoare'] / $last_month['volum']),2) }} EUR</h4>
+            @if ($last_month['valoare'] == 0)
+                <h4>Nu exista date</h4>
+            @else
+                <h4>{{ round(($last_month['valoare'] / $last_month['volum']),2) }} EUR</h4>
+            @endif
         </div>
         <div class="col-lg-3 text-center">
             <h3>Pret mediu luna anterioara/furnizor</h3>
-            @foreach ($data_last_month as $data)
-                <h4>{{ $data->supplier }} &mdash; {{ round((($data->valoare_factura + $data->transport)/$data->volum),2) }} EUR</h4>
-            @endforeach
+            @if ($data_last_month->count() == 0)
+                <h4>Nu exista date</h4>
+            @else
+                @foreach ($data_last_month as $data)
+                    <h4>{{ $data->supplier }} &mdash; {{ round((($data->valoare_factura + $data->transport)/$data->volum),2) }} EUR</h4>
+                @endforeach
+            @endif
         </div>
     </div>
 @stop
