390d9aecd857c40784d9d081d7407969e57e140b
Viorel Robu <viorel.robu@schweighofer.ro>
2019-11-28 10:05:46 +0200

    footer update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
     <div class="pull-right hidden-xs">
-      <b>Versiune</b> ALPHA 1.0
+      <b>Versiune</b> 1.0.0
     </div>
     <strong>Copyright &copy; 2019 <a href="">Viorel Robu</a>.</strong> All rights
     reserved.
