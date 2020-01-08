ba4b54de04da30b3fbf0c0ba0a32016afeae87df
Viorel Robu <viorel.robu@schweighofer.ro>
2020-01-08 11:02:54 +0200

    bugfix dashboard price calculation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -73,10 +73,22 @@ class HomeController extends Controller
         // Calculate average price current month
         $first_current_month = $now->copy()->firstOfMonth()->format('Y-m-d');
         $last_current_month = $now->copy()->lastOfMonth()->format('Y-m-d');
-        $data_current_month = $this->calculateAveragePrice($first_current_month, $last_current_month);
+        $volume_current_month = $this->getVolume($first_current_month, $last_current_month);
+        $value_current_month = $this->getValue($first_current_month, $last_current_month);
         $current_month = ['volum' => 0, 'valoare' => 0];
+        $data_current_month = $volume_current_month;
         foreach ($data_current_month as $data) {
+            foreach ($value_current_month as $value) {
+                if ($value->supplier == $data->supplier) {
+                    $data->valoare_factura = $value->valoare_factura;
+                    $data->transport = $value->transport;
+                }
+            }
+        }
+        foreach ($volume_current_month as $data) {
             $current_month['volum'] += $data->volum;
+        }
+        foreach ($value_current_month as $data) {
             $current_month['valoare'] += $data->valoare_factura + $data->transport;
         }
 
@@ -85,10 +97,22 @@ class HomeController extends Controller
         $last_month_first->startOfMonth()->format('Y-m-d');
         $last_month_last = new Carbon('last day of last month');
         $last_month_last->endOfMonth()->format('Y-m-d');
-        $data_last_month = $this->calculateAveragePrice($last_month_first, $last_month_last);
+        $volume_last_month = $this->getVolume($last_month_first, $last_month_last);
+        $value_last_month = $this->getValue($last_month_first, $last_month_last);
         $last_month = ['volum' => 0, 'valoare' => 0];
+        $data_last_month = $volume_last_month;
         foreach ($data_last_month as $data) {
+            foreach ($value_last_month as $value) {
+                if ($value->supplier == $data->supplier) {
+                    $data->valoare_factura = $value->valoare_factura;
+                    $data->transport = $value->transport;
+                }
+            }
+        }
+        foreach ($volume_last_month as $data) {
             $last_month['volum'] += $data->volum;
+        }
+        foreach ($value_last_month as $data) {
             $last_month['valoare'] += $data->valoare_factura + $data->transport;
         }
 
@@ -105,15 +129,43 @@ class HomeController extends Controller
                         'data_last_month' => $data_last_month,
                     ]);
     }
-
+    
     /**
-     * Calculate the average for timber for the external suppliers for a given time period
+     * Get the volume for the set time period
      * 
      * @param $from - beginning date
      * @param $to - end date
      * @return collection
      */
-    public function calculateAveragePrice($from, $to)
+    public function getValue($from, $to)
+    {
+        $company_id = session()->get('company_was_selected');
+
+        $nir_data = DB::table('nir')
+            ->join('suppliers', 'nir.supplier_id', '=', 'suppliers.id')
+            ->leftJoin('invoices', 'invoices.nir_id', '=', 'nir.id')
+            ->where('company_id', $company_id)
+            ->where('suppliers.supplier_group_id', 2)
+            ->whereBetween('data_nir', [$from, $to])
+            ->select([
+                'suppliers.name as supplier',
+                DB::raw('SUM(invoices.valoare_factura) as valoare_factura'),
+                DB::raw('SUM(invoices.valoare_transport) as transport')
+            ])
+            ->groupBy('suppliers.name')
+            ->get();
+
+        return $nir_data;
+    }
+
+    /**
+     * Get the value for the set time period
+     * 
+     * @param $from - beginning date
+     * @param $to - end date
+     * @return collection
+     */
+    public function getVolume($from, $to)
     {
         $company_id = session()->get('company_was_selected');
 
@@ -126,14 +178,11 @@ class HomeController extends Controller
             ->whereBetween('data_nir', [$from, $to])
             ->select([
                 'suppliers.name as supplier',
-                DB::raw('SUM(nir_details.volum_receptionat) as volum'),
-                DB::raw('SUM(invoices.valoare_factura) as valoare_factura'),
-                DB::raw('SUM(invoices.valoare_transport) as transport')
+                DB::raw('SUM(nir_details.volum_receptionat) as volum')
             ])
             ->groupBy('suppliers.name')
             ->get();
 
         return $nir_data;
     }
-
 }
