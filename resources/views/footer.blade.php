fb0e9862004aa10b6a0688bff682caec96a4cf3c
ViorelRobu <wwjd.vio@gmail.com>
2021-02-03 18:49:11 +0200

    change footer app version

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
     <div class="pull-right hidden-xs">
-      <b>Versiune</b> 1.3.0
+      <b>Versiune</b> 1.3.1
     </div>
     <strong>Copyright &copy; {{ Carbon\Carbon::now()->year }} <a href="">Viorel Robu</a>.</strong> All rights
     reserved.
