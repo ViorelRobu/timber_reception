90fb5e6350cc976d48bcc201ab9e8b7a4aa97766
ViorelRobu <wwjd.vio@gmail.com>
2021-02-03 18:18:08 +0200

    modified version in footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
     <div class="pull-right hidden-xs">
-      <b>Versiune</b> 1.1.0
+      <b>Versiune</b> 1.3.0
     </div>
     <strong>Copyright &copy; {{ Carbon\Carbon::now()->year }} <a href="">Viorel Robu</a>.</strong> All rights
     reserved.
